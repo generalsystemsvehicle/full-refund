ce5dad5f3518f2dfd0e668b50bb182da19bba8d1
Brian Wells <wells@users.noreply.github.com>
2020-04-14 09:28:29 -0500

    Create Redirects.php

---
+++ src/Api/Redirects.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace GeneralSystemsVehicle\VitalSource\Api;
+
+use Illuminate\Support\Arr;
+use GuzzleHttp\Exception\RequestException;
+use GeneralSystemsVehicle\VitalSource\Guzzle\Api;
+
+class Redirects extends Api
+{
+    /**
+     * Generate an SSO link to a book for a user.
+     *
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function sso($payload = [])
+    {
+        if (! Arr::has($payload, 'X-VitalSource-Access-Token')) {
+            return null;
+        }
+
+        if (! Arr::has($payload, 'destination')) {
+            return null;
+        }
+
+        // Destination - https://bookshelf.vitalsource.com/books/{vbid}
+        // Brand - bookshelf.vitalsource.com
+
+        return $this->try(function() use ($payload)
+        {
+            $body = '<?xml version="1.0" encoding="UTF-8"?>
+                <redirect>
+                    <destination>' . Arr::get($payload, 'destination') . '</destination>
+                    <brand>' . Arr::get($payload, 'brand', 'bookshelf.vitalsource.com') . '</brand>
+                </redirect>';
+
+            return $this->client->post('v3/redirects', [
+                'body' => $body,
+                'headers' => [
+                    'X-VitalSource-Access-Token' => Arr::get($payload, 'X-VitalSource-Access-Token'),
+                ],
+            ]);
+        });
+    }
+}
