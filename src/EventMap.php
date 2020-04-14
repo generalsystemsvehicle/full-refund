8aeba132ed241d192ab6c8eba1b8ac378aab89d4
Brian Wells <wells@users.noreply.github.com>
2020-04-14 09:23:02 -0500

    Create EventMap.php

---
+++ src/EventMap.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GeneralSystemsVehicle\VitalSource;
+
+trait EventMap
+{
+    /**
+     * All of the package event / listener mappings.
+     *
+     * @var array
+     */
+    protected $events = [
+        //
+    ];
+}
