73fcdbabcf0f877d938e56b5c1c175f51aa28c91
Brian Wells <wells@users.noreply.github.com>
2020-04-14 09:26:48 -0500

    Create Products.php

---
+++ src/Api/Products.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace GeneralSystemsVehicle\VitalSource\Api;
+
+use GeneralSystemsVehicle\VitalSource\Guzzle\Api;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Arr;
+
+class Products extends Api
+{
+    /**
+     * Get the products index.
+     *
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function index($payload = [])
+    {
+        return $this->try(function() use ($payload)
+        {
+            return $this->client->get('v4/products', [
+                'query' => Arr::get($payload, 'query', []),
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+            ]);
+        });
+    }
+
+
+    /**
+     * Get a product.
+     *
+     * @param  string $vbid
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function get($vbid, $payload = [])
+    {
+        // Optional query parameter:
+        // 'include_details' => 'identifiers,metadata,subjects,accessibility'
+
+        return $this->try(function() use ($vbid, $payload)
+        {
+            return $this->client->get('v4/products/' . $vbid, [
+                'query' => Arr::get($payload, 'query', []),
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+            ]);
+        });
+    }
+
+    /**
+     * Get the table of contents for a product.
+     *
+     * @param  string $vbid
+     * @return array|null
+     */
+    public function getTableOfContents($vbid)
+    {
+        return $this->try(function() use ($vbid)
+        {
+            return $this->client->get('v4/products/' . $vbid . '/toc', [
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+            ]);
+        });
+    }
+}
